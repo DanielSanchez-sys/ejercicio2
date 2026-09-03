9b29acca085d76268049ecb10586ba762b6cb929
Daniel <danel78178@gmail.com>
2026-09-03 10:08:57 -0400

    Agregando funcion de potencia

---
+++ aritmetica.py
@@ -0,0 +1,9 @@
+def potencia(base, exponente):
+    return base ** exponente
+
+if __name__ == "__main__":
+    b = float(input("Ingresa la base: "))
+    e = float(input("Ingresa el exponente: "))
+    print(f"Resultado: {potencia(b, e)}")
+
+# JOSE DANIEL SANCHEZ MAMANI
